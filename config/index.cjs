debe38112cc8544d6142fe473259c26f1df2cf21
hajdvnobs <126544576+hajdvnobs@users.noreply.github.com>
2023-02-28 13:37:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oG1xV6DK24unHS-nnTLFQSuvWIhg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '6UxfpRXK3ETnKTJmCqHgd6P2Fui5ihi_b7Nydu0kfMY',
+      useTemplateId: 'oG1xV6OXsvl11PbyEAKXPL8AE7hI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
